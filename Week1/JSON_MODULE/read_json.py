d826e127250417e536b5261010beab0bdecb0a75
Areeba shamsi <142543048+areebashamsii@users.noreply.github.com>
2025-10-27 07:30:09 +0500

    updated

--- Week1/JSON_MODULE/read_json.py
+++ Week1/JSON_MODULE/read_json.py
@@ -13,7 +13,7 @@ product_info={  # python dictionary
 }
 
 #coverting and storing it into a JSON file
-with open("data.json","w") as product_info_json:
+with open(r"C:\Users\USER\Documents\Python\Week1\JSON_MODULE\data.json","w") as product_info_json:
     json.dump(product_info,product_info_json,indent=2) 
     
       ## Explaination ##
@@ -23,24 +23,30 @@ with open("data.json","w") as product_info_json:
 #json.dump() to convert python dict into a json str
 #json.dump(dict_name,jason_file_name, indent=value) # indent=4 will give 4 space after every level for the readabilty
  
+ #updating key
+
+with open(r"C:\Users\USER\Documents\Python\Week1\JSON_MODULE\data.json","w") as product_info_json:
+    json.dump(product_info,product_info_json,indent=2) 
+product_info["Headphone"]=product_info["headphone"]
+
+
  # Adding value in python dic:
   
-product_info["Mouse"]={"Name":"Lenovo","Price":4000}
 
-with open("data.json","w") as product_info_json:
+with open(r"C:\Users\USER\Documents\Python\Week1\JSON_MODULE\data.json","w") as product_info_json:
     json.dump(product_info,product_info_json,indent=2) 
     
 # We cant direclty add value in json str so we do the above step.
 
 ## delete a key value pair from the JSON file
 
-with open("data.json","r") as product_info_json: # to read an existing jason file
+with open(r"C:\Users\USER\Documents\Python\Week1\JSON_MODULE\data.json","r") as product_info_json: # to read an existing jason file
     product_info = json.load(product_info_json) #to load a jason file or to convert it into a python dic for updation or deletion
 product_info.pop("Cable",None) #dict_name.pop("key",None) is to delete a value from an existing dict
 
 #save the updated dict into json
 with open("c:/Users/USER/Documents/Python/Week1/JSON_MODULE/data.json","w") as product_info_json:
-    json.dump(product_info,product_info_json,indent=3)
+    json.dump(product_info,product_info_json,indent=3,sort_keys=True) # sort_keys=True will sort the keys alphabatically
 print(product_info)
 
 ## Converting 3 different i.e nested JSON str into separate python dict:
@@ -58,3 +64,31 @@ uni_faculty=uni_info["staff_info"]["uni_faculty"]
 print("Studnet Info:",student_info)
 print("visiting faculty:",visiting_faculty)
 print("University faculty",uni_faculty)
+
+## dumps over dump use: ( dump store the json str in file where as dumps only convert 
+# the dict into json str but donot store in a seperate file) ##
+
+items={
+    "Car":{"name":"Nissan GTR"},
+    "Bike":{"name":"Yamaha"},
+    "SUV":{"name":"Fortuner"},
+
+} 
+print(items)
+print(type(items))
+# convert into json str
+item_json=json.dumps(items) #convert the python dic into json str but won't store in file ,is shareable and good for API
+print(item_json)
+print(type(item_json))
+
+## loads over load
+# loads is used to read json str which is store in the code as python dict and not in the separate json file
+
+item_dict=json.loads(item_json)
+print(item_dict)
+print(type(item_dict)) #converted again into a python dict
+
+## .get , it sets the default value of the key so that if value doesn't exist the keyError not appear insted the defualt value will print
+
+#print(items("Bicycle"))                     #will through an error
+print(items.get("Bicycle", "Not Found!")) #.gets only work in python dict so first convert json into py dict.
