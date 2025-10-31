661fbe9618a2e268d06f07fd4bef78b1d573de54
Areeba shamsi <142543048+areebashamsii@users.noreply.github.com>
2025-10-31 22:14:43 +0500

    update_stu function work has been done.

--- Week1/Student_management_system/stu_ops.py
+++ Week1/Student_management_system/stu_ops.py
@@ -1,16 +1,18 @@
 from file_ops import read_data, store_data
 
+filename=r"C:\Users\USER\Documents\Python\Week1\Student_management_system\records.json"
+
 def add_stu():
     while True:
         id = input("Enter your Id:")
 
         if len(id) == 4 and id.isdigit():
-            id = int(id)
+            
             break
         else:
             print("You Enter Invalid Id! ID format is : 0008")
 
-    records = read_data(r"C:\Users\USER\Documents\Python\Week1\Student_management_system\records.json")
+    records = read_data(filename)
 
     for student in records:
         if student['Id'] == id:
@@ -30,8 +32,37 @@ def add_stu():
     }
 
     records.append(stu_info)
-    store_data(r"C:\Users\USER\Documents\Python\Week1\Student_management_system\records.json", records)
+    store_data(filename, records)
 
     print("Student added successfully!")
 
-add_stu()
+def update_stu():
+    records = read_data(filename)
+    update_id=input("Enter Student Id to update your record:")
+    
+    
+    for student in records:
+        
+        if student["Id"] == update_id:
+                
+           update_name=input("Enter New Name:")
+           update_age=input("Enter New Age:")
+           update_department=input("Enter New Department:") 
+
+           student["Id"]=update_id
+           student["Name"]=update_name
+           student["Age"]=update_age
+           student["Department"]=update_department
+            
+           store_data(filename, records)
+
+           print("Record updated successfully!")
+           break
+    else:
+          print("This Id does not exist!")
+          update_stu()
+
+
+# add_stu()
+#update_stu()
+
