e8b80caa0b9d6807219ffb16e4b8fc01e3bcd978
Areeba shamsi <142543048+areebashamsii@users.noreply.github.com>
2025-11-01 13:35:55 +0500

    delete_stu function work has been done.

--- Week1/Student_management_system/stu_ops.py
+++ Week1/Student_management_system/stu_ops.py
@@ -3,6 +3,7 @@ from file_ops import read_data, store_data
 filename=r"C:\Users\USER\Documents\Python\Week1\Student_management_system\records.json"
 
 def add_stu():
+    records = read_data(filename)
     while True:
         id = input("Enter your Id:")
 
@@ -12,7 +13,7 @@ def add_stu():
         else:
             print("You Enter Invalid Id! ID format is : 0008")
 
-    records = read_data(filename)
+   
 
     for student in records:
         if student['Id'] == id:
@@ -40,7 +41,6 @@ def update_stu():
     records = read_data(filename)
     update_id=input("Enter Student Id to update your record:")
     
-    
     for student in records:
         
         if student["Id"] == update_id:
@@ -59,10 +59,37 @@ def update_stu():
            print("Record updated successfully!")
            break
     else:
-          print("This Id does not exist!")
-          update_stu()
+        print("This Id does not exist!")
+        update_stu()
+
+def delete_stu():
+    records = read_data(filename)
+    del_id=input("Enter Student Id to delete the record:")
+    
+    for student in records:
+        
+        if student["Id"] == del_id:
+            confrm=input("Are you Sure to delete your record? (Yes or No)")
+            
+            if confrm.lower()=="yes":
+                   
+                   records.remove(student)
+                   store_data(filename, records)
+                   print("Record Deleted Successfully!")
+
+            else:
+                print("Invalid Input! Enter yes or No")
+            break
+         
+                
+            
+    else:
+     print("This Id does not Exist!")
 
 
-# add_stu()
+
+
+#add_stu()
 #update_stu()
+#delete_stu()
 
