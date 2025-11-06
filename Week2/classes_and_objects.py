6683909098229a56d25fa734bb4b9084402e9c51
Areeba shamsi <142543048+areebashamsii@users.noreply.github.com>
2025-11-06 13:33:34 +0500

    updated

--- Week2/classes_and_objects.py
+++ Week2/classes_and_objects.py
@@ -1,7 +1,16 @@
 ## Class is a blueprint of object properties: ##
+
+#Creating an empty class:
+
+class person():
+    pass # this keyword is used to create an empty class , not to get the error.
+
+
+
+## Creating a pateint class ##
 class pateint():
     def __init__(self,pateint_id,pateint_name,age,disease,bill_amount,admit_date,check_out_date):
-       
+       # __init__ is a special method use to create a constructor , it calls automatically when you iniialze an object
        self.pateint_id=pateint_id
        self.pateint_name=pateint_name
        self.age=age
@@ -24,6 +33,8 @@ class pateint():
 
 ## Creating object using class ##
 
+      # object is an entity create by using a class.
+
 p1=pateint(101,"Saleem",45,"fever",2300,"2025-06-21","2025-06-22")
 p1.pateint_info()
 
@@ -40,4 +51,18 @@ p2.pateint_info()
 p3.pateint_info()
 p4.pateint_info()
 p5.pateint_info()
+
+
+## Deleting an object
+
+del p1 
+# print(p1) #it will thorugh an error as p1 is deleted
+print(p2) # (<__main__.pateint object at 0x00000183CB618CD0>) it will show the object like this as you cannot print an object directly using print().
+
+## Update Object ##
+
+p3.pateint_info()
+p3.bill_amount=7000
+p3.pateint_info()
+
 
